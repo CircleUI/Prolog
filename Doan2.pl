c80357c7dc3ba62a0e1eb386443fa15e39f6ac1b
NhatHuyTran <nhathuy13598@gmail.com>
2018-11-10 10:04:06 +0700

    20 cau hoi va loi giai. Co 2 cau lam sai

--- Doan2.pl
+++ Doan2.pl
@@ -1,6 +1,4 @@
-/**
- *male
-/
+/**male*/
 male(pPhillip).
 male(pCharles).
 male(cmPhillips).
@@ -9,14 +7,12 @@ male(pAndrew).
 male(pEdward).
 male(pWilliam).
 male(pHarry).
-male(PeterPhillips).
+male(peterPhillips).
 male(mTindall).
 male(jvSevern).
 male(pGeorge).
 male(mgTindall).
-/**
-*female
-/
+/**female*/
 female(qElizabethII).
 female(pDiana).
 female(cpBowles).
@@ -33,11 +29,9 @@ female(pCharlotte).
 female(sPhillips).
 female(iPhillips).
 
-/**
-*parent
-/
-/**The he 1
-/
+/**parent*/
+
+/**The he 1*/
 parent(qElizabethII,pCharles).
 parent(pPhillip,pCharles).
 parent(qElizabethII,pAnne).
@@ -46,15 +40,14 @@ parent(qElizabethII,pAndrew).
 parent(pPhillip,pAndrew).
 parent(qElizabethII,pEdward).
 parent(pPhillip,pEdward).
-/**The he 2
-/
+/**The he 2*/
 parent(pDiana,pWilliam).
 parent(pCharles,pWilliam).
 parent(pDiana,pHarry).
 parent(pCharles,pHarry).
 
-parent(cmPhillips,PeterPhillips).
-parent(pAnne,PeterPhillips).
+parent(cmPhillips,peterPhillips).
+parent(pAnne,peterPhillips).
 parent(cmPhillips,zPhillips).
 parent(pAnne,zPhillips).
 
@@ -68,39 +61,54 @@ parent(pEdward,jvSevern).
 parent(srJones,llmWindsor).
 parent(pEdward,llmWindsor).
 
+/**The he 3*/
 parent(pWilliam,pGeorge).
 parent(kMiddleton,pGeorge).
 parent(pWilliam,pCharlotte).
 parent(kMiddleton,pCharlotte).
 
 parent(aKelly,sPhillips).
-parent(PeterPhillips,sPhillips).
+parent(peterPhillips,sPhillips).
 parent(aKelly,iPhillips).
-parent(PeterPhillips,iPhillips).
+parent(peterPhillips,iPhillips).
 
 parent(zPhillips,mgTindall).
 parent(mTindall,mgTindall).
 
-/**
-*married
-/
+/**married*/
 
 married(qElizabethII,pPhillips).
+married(pPhillips,qElizabethII).
+
 married(pCharles,cpBowles).
+married(cpBowles,pCharles).
+
 married(pAnne,tLaurence).
+married(tLaurence,pAnne).
+
 married(srJones,pEdward).
+married(pEdward,srJones).
+
 married(pWilliam,kMiddleton).
-married(aKelly,PeterPhillips).
+married(kMiddleton,pWilliam).
+
+married(aKelly,peterPhillips).
+married(peterPhillips,aKelly).
+
 married(zPhillips,mTindall).
+married(mTindall,zPhillips).
 
-/**
-*divorced
-/
+/**divorced*/
 divorced(pDiana,pCharles).
+divorced(pCharles,pDiana).
+
 divorced(cmPhillips,pAnne).
+divorced(pAnne,cmPhillips).
+
 divorced(sFerguson,pAndrew).
+divorced(pAndrew,sFerguson).
 
-
+/**Kiem tra dung sai*/
 husband(Person,Wife):-married(Person,Wife),male(Person).
 wife(Person,Husband):-married(Person,Husband),female(Person).
 father(Parent,Child):-parent(Parent,Child),male(Parent).
@@ -108,13 +116,16 @@ mother(Parent,Child):-parent(Parent,Child),female(Parent).
 child(Child,Parent):-parent(Parent,Child).
 son(Child,Parent):-parent(Parent,Child),male(Child).
 daughter(Child,Parent):-parent(Parent,Child),female(Child).
+
+/**Kiem tra chau*/
 grandparent(GP,GC):-parent(X,GC),parent(GP,X).
-grandmother(GM,GC):-parent(X,GC),parent(GM,X),female(GM).
-grandfather(GF,GC):-parent(X,GC),parent(GF,X),male(GF).
+grandmother(GM,GC):-mother(X,GC),mother(GM,X).
+grandfather(GF,GC):-father(X,GC),father(GF,X).
 grandchild(GC,GP):-grandparent(GP,GC).
-grandson(GS,GP):-grandchild(GP,GS),male(GS).
-granddaughter(GD,GP):-grandchild(GP,GD),female(GD).
-sibling(Person1,Person2):-parent(X,Person1),parent(X,Person2).
+grandson(GS,GP):-grandchild(GS,GP),male(GS).
+granddaughter(GD,GP):-grandchild(GD,GP),female(GD).
+
+sibling(Person1,Person2):-father(X,Person1),child(Person2,X).
 brother(Person,Sibling):-sibling(Person,Sibling),male(Person).
 sister(Person,Sibling):-sibling(Person,Sibling),female(Person).
 aunt(Person,NieceNephew):-parent(X,NieceNephew),sibling(X,Person),female(Person).
